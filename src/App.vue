0b92c527f48f493827aa9b079b92cb190aba3d3e
davidvidovic-web <mail@davidvidovic.com>
2023-12-07 11:03:30 +0100

    added analytics

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,6 @@ import { RouterLink, RouterView } from 'vue-router'
 import HomeAbout from './components/HomeAbout.vue'
 import Social from './components/Social.vue'
 </script>
-
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
